a85055db3de12f0fb3c9c539d109f990fa95f6ef
Adrita27 <rahmanadrita27@gmail.com>
2022-03-27 01:26:24 +0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            BUSHRA b1 = new BUSHRA();
+            b1.SetName("ADRITA RAHMAN BUSHRA");
+            b1.SetId("20-43367-1");
+            b1.SetCgpa(3.76);
+            b1.SetProgramme("Computer Science");
+            b1.SetUniversity("AMERICAN INTERNATIONAL UNIVERSITY BANGLADESH");
+            b1.display();
+
+
+
+
+            BUSHRA b2 = new BUSHRA("ADRITA RAHMAN BUSHRA", "20-43367-1", 3.76,"AMERICAN INTERNATIONAL UNIVERSITY BANGLADESH" ,"Computer Science" );
+            b2.display();
+            BUSHRA b3 = new BUSHRA(b1);
+
+            b3.display();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
